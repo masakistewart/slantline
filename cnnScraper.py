e8029ac6da2c011dcf560c86d54a450eff5a8ea9
masakistewart <stewartcairo@gmail.com>
2016-03-14 21:13:32 +0000

    scrape cnn headlines 50 results at a time

---
+++ cnnScraper.py
@@ -0,0 +1,9 @@
+from bs4 import BeautifulSoup
+import requests
+
+cnnRaw = requests.get("http://www.cnn.com/specials/last-50-stories")
+
+cnnSoup = BeautifulSoup(cnnRaw.content, "html5lib")
+headlines = cnnSoup.find_all("h3", {"class": "cd__headline"})
+for headline in headlines:
+    print(headline.text)
